22ac0334999d1c817ea48b2588f21167483311cb
Elecon-rou <quest90spin@gmail.com>
2016-04-08 09:47:36 +0000

    cosmetic changes in build system

---
+++ config.mk
@@ -0,0 +1,21 @@
+
+# compiler and linker
+CC=gcc
+
+# flags
+CPPFLAGS=-Dlinux -D_XOPEN_SOURCE=600 -D_POSIX_SOURCE -DPOSIX_SOURCE=199309L -D_BSD_SOURCE -D_GNU_SOURCE -DUSECMS 
+CFLAGS=-pipe -Os -mtune=generic -Wall -fno-strength-reduce -nodefaultlibs -fno-strict-aliasing -ffunction-sections -fdata-sections $(INCS)
+LDFLAGS=-Wl,--gc-sections,--sort-common,-s $(LIBS)
+
+# install paths 
+PREFIX=/usr
+LIBDIR=${PREFIX}/lib
+INCDIR=${PREFIX}/include/X11
+
+# includes and libs 
+LIBS = -L/usr/lib
+INCS = -I. -I../include -I../include/X11 -I../libxtrans
+
+#If you do not have setlocale, set this:
+#CPPFLAGS+= -DX_LOCALE -DXLOCALEDIR=\"/usr/share/locale\" 
+KEYSYMDB=${PREFIX}/share/X11/XKeysymDB
